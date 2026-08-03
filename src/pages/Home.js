2d09670dab1cb41818ad8483527aa9d1300d00d8
K.Varshitha <varshithakandregula2008@gmail.com>
2026-08-03 16:18:35 +0530

    hg

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import henryImage from '../assets/henry.png';
+import henryImage from '../assets/Events/henry.png';
 import Gallery from '../components/Gallery';
 import './Home.css';
 
